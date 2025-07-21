6ed205139b9039d854f451600737caf70cb86838
Vybhav Achar Bhargav <vybhavab@gmail.com>
2025-07-20 17:49:43 -0700

    add touch to exit

--- selfdrive/ui/tmux_viewer.cc
+++ selfdrive/ui/tmux_viewer.cc
@@ -18,6 +18,7 @@ const int MAX_LINES = (SCREEN_HEIGHT - 100) / LINE_HEIGHT;
 std::string tmux_session = "default";
 std::string tmux_content;
 bool running = true;
+bool session_exists = true;
 
 std::string runCommand(const std::string &cmd) {
     std::string temp_file = "/tmp/tmux_raylib_output.txt";
@@ -43,19 +44,29 @@ std::string runCommand(const std::string &cmd) {
 
 void updateTmuxContent() {
     while (running) {
+        std::string check_cmd = "tmux has-session -t " + tmux_session + " 2>/dev/null";
+        if (std::system(check_cmd.c_str()) != 0) {
+            session_exists = false;
+            running = false;
+            break;
+        }
+        
         std::string cmd = "tmux capture-pane -t " + tmux_session + " -e -p";
         tmux_content = runCommand(cmd);
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
 }
 
-void drawText(const std::string &text, int x, int y, int fontSize, Color color) {
-    DrawText(text.c_str(), x, y, fontSize, color);
-}
+
 
 int main() {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Tmux Viewer");
     SetTargetFPS(30);
+    
+    Font font = LoadFontEx("third_party/raylib/examples/text/resources/fonts/jupiter_crash.png", 20, 0, 250);
+    if (font.texture.id == 0) {
+        font = GetFontDefault();
+    }
     
     std::string check_cmd = "tmux has-session -t " + tmux_session + " 2>/dev/null";
     if (std::system(check_cmd.c_str()) != 0) {
@@ -65,31 +76,43 @@ int main() {
     
     std::thread update_thread(updateTmuxContent);
     
-    while (!WindowShouldClose()) {
+    while (!WindowShouldClose() && session_exists) {
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+            Vector2 mousePos = GetMousePosition();
+            if (mousePos.y < 100) {
+                break;
+            }
+        }
+        
         BeginDrawing();
         ClearBackground(BLACK);
         
-        drawText("Tmux Viewer - Session: " + tmux_session, 20, 20, FONT_SIZE, WHITE);
-        drawText("Press ESC to exit", 20, SCREEN_HEIGHT - 40, 20, GRAY);
+        DrawTextEx(font, ("Tmux Session: " + tmux_session).c_str(), {20, 20}, 24, 1, WHITE);
+        DrawTextEx(font, "Touch top area to exit", {20, SCREEN_HEIGHT - 40}, 16, 1, GRAY);
         
-        std::istringstream stream(tmux_content);
-        std::string line;
-        int line_num = 0;
-        int y_offset = 80;
-        
-        while (std::getline(stream, line) && line_num < MAX_LINES) {
-            if (line.length() > 120) {
-                line = line.substr(0, 120) + "...";
+        if (!session_exists) {
+            DrawTextEx(font, "Session closed - exiting...", {20, 60}, 20, 1, RED);
+        } else {
+            std::istringstream stream(tmux_content);
+            std::string line;
+            int line_num = 0;
+            int y_offset = 60;
+            
+            while (std::getline(stream, line) && line_num < MAX_LINES) {
+                if (line.length() > 100) {
+                    line = line.substr(0, 100);
+                }
+                
+                for (char &c : line) {
+                    if (c < 32 && c != '\t') c = ' ';
+                }
+                
+                DrawTextEx(font, line.c_str(), {20, (float)(y_offset + (line_num * LINE_HEIGHT))}, 18, 1, WHITE);
+                line_num++;
             }
-            drawText(line, 20, y_offset + (line_num * LINE_HEIGHT), 20, WHITE);
-            line_num++;
         }
         
         EndDrawing();
-        
-        if (IsKeyPressed(KEY_ESCAPE)) {
-            break;
-        }
     }
     
     running = false;
